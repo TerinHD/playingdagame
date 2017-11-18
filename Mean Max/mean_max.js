e5a21ebc5530a564b0c1d1f684a8518c46457660
Nathan Gewecke <terinhd@gmail.com>
2017-11-18 00:35:33 -0500

    Adds baseline for other vehicles

--- Mean Max/mean_max.js
+++ Mean Max/mean_max.js
@@ -14,12 +14,165 @@ function Reaper() {
     var yVel = 0;
 }
 
+function Destroyer() {
+    var unitId = -1;
+    var playerId = -1;
+    var mass = -1;
+    var radius = -1;
+    var xPos = 0;
+    var yPos = 0;
+    var xVel = 0;
+    var yVel = 0;
+}
+
+function Doof() {
+    var unitId = -1;
+    var playerId = -1;
+    var mass = -1;
+    var radius = -1;
+    var xPos = 0;
+    var yPos = 0;
+    var xVel = 0;
+    var yVel = 0;
+}
+
+function Tanker() {
+    var unitId = -1;
+    var mass = -1;
+    var radius = -1;
+    var xPos = 0;
+    var yPos = 0;
+    var xVel = 0;
+    var yVel = 0;
+    var waterQuantity = -1;
+    var waterCapacity = -1;
+}
+
 function Wreck() {
     var unitId = -1;
     var radius = -1;
     var xPos = 0;
     var yPos =0;
     var waterQuantity = -1;
+}
+
+function createUpdateReaper( unitId, inputs ) {
+    var tmpReaper;
+    if( reapers[unitId] ) {
+        tmpReaper = reapers[unitId];
+    } else {
+        tmpReaper = new Reaper();
+        tmpReaper.unitId = unitId;
+        reapers[unitId] = tmpReaper;
+    }
+    
+    tmpReaper.playerId = parseInt(inputs[2]);
+    
+    if( tmpReaper.playerId == 0 && !myReaper) {
+        myReaper = tmpReaper;
+    }
+    
+    tmpReaper.mass = parseFloat(inputs[3]);
+    tmpReaper.radius = parseInt(inputs[4]);
+    tmpReaper.xPos = parseInt(inputs[5]);
+    tmpReaper.yPos = parseInt(inputs[6]);
+    tmpReaper.xVel = parseInt(inputs[7]);
+    tmpReaper.yVel = parseInt(inputs[8]);  
+    
+    return tmpReaper;
+}
+
+function createUpdateDestroyer( unitId, inputs ) {
+    var tmpDestroyer;
+    if( destroyers[unitId] ) {
+        tmpDestroyer = destroyers[unitId];
+    } else {
+        tmpDestroyer = new Destroyer();
+        tmpDestroyer.unitId = unitId;
+        destroyers[unitId] = tmpDestroyer;
+    }
+    
+    tmpDestroyer.playerId = parseInt(inputs[2]);
+    
+    if( tmpDestroyer.playerId == 0 && !myDestroyer) {
+        myDestroyer = tmpDestroyer;
+    }
+    
+    tmpDestroyer.mass = parseFloat(inputs[3]);
+    tmpDestroyer.radius = parseInt(inputs[4]);
+    tmpDestroyer.xPos = parseInt(inputs[5]);
+    tmpDestroyer.yPos = parseInt(inputs[6]);
+    tmpDestroyer.xVel = parseInt(inputs[7]);
+    tmpDestroyer.yVel = parseInt(inputs[8]);
+    
+    return tmpDestroyer;
+}
+
+function createUpdateDoof( unitId, inputs ) {
+    var tmpDoof;
+    if( doofs[unitId] ) {
+        tmpDoof = doofs[unitId];
+    } else {
+        tmpDoof = new Doof();
+        tmpDoof.unitId = unitId;
+        doofs[unitId] = tmpDoof;
+    }
+    
+    tmpDoof.playerId = parseInt(inputs[2]);
+    
+    if( tmpDoof.playerId == 0 && !myDoof) {
+        myDoof = tmpDoof;
+    }
+    
+    tmpDoof.mass = parseFloat(inputs[3]);
+    tmpDoof.radius = parseInt(inputs[4]);
+    tmpDoof.xPos = parseInt(inputs[5]);
+    tmpDoof.yPos = parseInt(inputs[6]);
+    tmpDoof.xVel = parseInt(inputs[7]);
+    tmpDoof.yVel = parseInt(inputs[8]);
+    
+    return tmpDoof;
+}
+
+function createUpdateTanker( unitId, inputs ) {
+    var tmpTanker;
+    if( tankers[unitId] ) {
+        tmpTanker = tankers[unitId];
+    } else {
+        tmpTanker = new Tanker();
+        tmpTanker.unitId = unitId;
+        tankers[unitId] = tmpTanker;
+    }
+    
+    tmpTanker.mass = parseFloat(inputs[3]);
+    tmpTanker.radius = parseInt(inputs[4]);
+    tmpTanker.xPos = parseInt(inputs[5]);
+    tmpTanker.yPos = parseInt(inputs[6]);
+    tmpTanker.xVel = parseInt(inputs[7]);
+    tmpTanker.yVel = parseInt(inputs[8]);
+    tmpTanker.waterQuantity = parseInt(inputs[9]);
+    tmpTanker.waterCapacity = parseInt(inputs[10]);
+    
+    return tmpTanker;
+}
+
+
+function createUpdateWreck( unitId, inputs ) {
+    var tmpWreck;
+    if( wrecks[unitId]) {
+        tmpWreck = wrecks[unitId];   
+    } else {
+        tmpWreck = new Wreck();
+        tmpWreck.unitId = unitId;
+        wrecks[unitId] = tmpWreck;
+    }
+    
+    tmpWreck.radius = parseInt(inputs[4]);
+    tmpWreck.xPos = parseInt(inputs[5]);
+    tmpWreck.yPos = parseInt(inputs[6]);
+    tmpWreck.waterQuantity = parseInt(inputs[9]);
+    
+    return tmpWreck;
 }
 
 function checkInsideRadius( wreck, reaper ) {
@@ -31,21 +184,32 @@ function checkInsideRadius( wreck, reaper ) {
     return false;
 }
 
-function stopReaper( reaper ) {
-    var xDir = reaper.xPos - reaper.xVel;
-    var yDir = reaper.yPos - reaper.yVel;
+function stopVehicle( vehicle ) {
+    var xDir = vehicle.xPos - vehicle.xVel;
+    var yDir = vehicle.yPos - vehicle.yVel;
     return '' + xDir + ' ' + yDir + ' 150';
 }
 
+// Vehicle Lists
 var reapers = {};
+var destroyers = {};
+var doofs = {};
+var tankers = {};
+
+// Wrecks
 var wrecks = {};
+
+// My Vehicles
 var myReaper;
+var myDestroyer;
+var myDoof;
 
 // game loop
 while (true) {
     
-    var movement = "";
-    
+    var reaperMovement = 'WAIT';
+    var destroyerMovement = 'WAIT';
+    var doofMovement = 'WAIT';
     
     var myScore = parseInt(readline());
     var enemyScore1 = parseInt(readline());
@@ -58,58 +222,37 @@ while (true) {
     var unitCount = parseInt(readline());
     
     var topWaterQuantity = 0;
-    var topWreckId = 0;
+    var topWreckId = -1;
     
-    var insideWreck = false;
+    // Clear Tankers
+    tankers = {};
+    var lastTankerId = -1;
+    
+    var reaperInsideWreck = false;
 
     for (var i = 0; i < unitCount; i++) {
         var inputs = readline().split(' ');
         var unitId = parseInt(inputs[0]);
         var unitType = parseInt(inputs[1]);
         if( unitType == 0 ) {
-            var tmpReaper;
-            if( reapers[unitId] ) {
-                tmpReaper = reapers[unitId];
-            } else {
-                tmpReaper = new Reaper();
-                tmpReaper.unitId = unitId;
-                reapers[unitId] = tmpReaper;
-            }
-            
-            tmpReaper.playerId = parseInt(inputs[2]);
-            
-            if( tmpReaper.playerId == 0 && !myReaper) {
-                myReaper = tmpReaper;
-            }
-            
-            tmpReaper.mass = parseFloat(inputs[3]);
-            tmpReaper.radius = parseInt(inputs[4]);
-            tmpReaper.xPos = parseInt(inputs[5]);
-            tmpReaper.yPos = parseInt(inputs[6]);
-            tmpReaper.xVel = parseInt(inputs[7]);
-            tmpReaper.yVel = parseInt(inputs[8]);
+            createUpdateReaper( unitId, inputs );
+        } else if( unitType == 1) {
+            createUpdateDestroyer( unitId, inputs );
+        } else if( unitType == 2) {
+            createUpdateDoof( unitId, inputs );
+        } else if( unitType == 3) {
+            createUpdateTanker( unitId, inputs );
+            lastTankerId = unitId;
         } else if( unitType == 4 ) {
-            var tmpWreck;
-            if( wrecks[unitId]) {
-                tmpWreck = wrecks[unitId];   
-            } else {
-                tmpWreck = new Wreck();
-                tmpWreck.unitId = unitId;
-                wrecks[unitId] = tmpWreck;
-            }
-            
-            tmpWreck.radius = parseInt(inputs[4]);
-            tmpWreck.xPos = parseInt(inputs[5]);
-            tmpWreck.yPos = parseInt(inputs[6]);
-            tmpWreck.waterQuantity = parseInt(inputs[9]);
+            var tmpWreck = createUpdateWreck( unitId, inputs );
             
             if( tmpWreck.waterQuantity > topWaterQuantity ) {
                 topWreckId = unitId;
                 topWaterQuantity = tmpWreck.waterQuantity;
             }
             
-            if( checkInsideRadius( tmpWreck, myReaper ) && !insideWreck) {
-                insideWreck = true;   
+            if( checkInsideRadius( tmpWreck, myReaper ) && !reaperInsideWreck) {
+                reaperInsideWreck = true;   
             }
         }
 
@@ -124,15 +267,26 @@ while (true) {
         // var extra2 = parseInt(inputs[10]);
     }
     
-    if( insideWreck ) {
+    if( reaperInsideWreck ) {
         if( myReaper.xVel == 0 && myReaper.yVel == 0 ) {
-            movement = 'WAIT';   
+            reaperMovement = 'WAIT';   
         } else {
-            movement = stopReaper( myReaper );   
+            reaperMovement = stopVehicle( myReaper );   
         }
     } else {
-        var tmpWreck = wrecks[topWreckId];
-        movement = '' + tmpWreck.xPos + ' ' + tmpWreck.yPos + ' 150';
+        if( topWreckId !== -1 ) {
+            var tmpWreck = wrecks[topWreckId];
+            reaperMovement = '' + tmpWreck.xPos + ' ' + tmpWreck.yPos + ' 150';
+        } else {
+            reaperMovement = stopVehicle( myReaper );
+        }
+    }
+    
+    if( lastTankerId != -1 ) {
+        var tanker = tankers[lastTankerId];
+        destroyerMovement = '' + tanker.xPos + ' ' + tanker.yPos + ' 299'; 
+    } else {
+        destroyerMovement = stopVehicle(myDestroyer);
     }
 
     // Write an action using print()
@@ -140,7 +294,7 @@ while (true) {
     
     // Calc Fastest route to top quantity 
 
-    print( movement );
-    print( 'WAIT' );
-    print( 'WAIT' );
+    print( reaperMovement );
+    print( destroyerMovement );
+    print( doofMovement );
 }
